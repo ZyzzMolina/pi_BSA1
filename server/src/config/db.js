ddcd3bbfc5bad75442613df3ca17c607cce8d427
AxelAlejandroReséndizÁvila <124049803@upq.edu.mx>
2026-04-14 22:25:29 -0600

    cambios

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -6,9 +6,9 @@ dotenv.config();
 const pool = new pg.Pool({
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '5432'),
-  database: process.env.DB_NAME || 'sistema_academico',
+  database: process.env.DB_NAME || 'sistema_academico_db',
   user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || 'root',
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
